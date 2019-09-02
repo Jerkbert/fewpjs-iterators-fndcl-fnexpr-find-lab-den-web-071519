628a1cc8f2af5819baeff5a97be035920c98cacd
Jerkbert <raballantyne@gmail.com>
2019-09-02 15:13:36 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objArray) {
+  let winner = objArray.find(function(game) {
+    return game.result === "W"
+  })
+  return winner ? winner.year : undefined
+}
